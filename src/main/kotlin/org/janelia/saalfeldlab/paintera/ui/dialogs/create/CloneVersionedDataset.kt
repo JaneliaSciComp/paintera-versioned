3773cce91ae89860360510b2a8ee5bfca0ed357f
mzouink <zouinkhi.marwan@gmail.com>
2022-08-26 19:20:33 +0100

    clone dataset

--- src/main/kotlin/org/janelia/saalfeldlab/paintera/ui/dialogs/create/CloneVersionedDataset.kt
+++ src/main/kotlin/org/janelia/saalfeldlab/paintera/ui/dialogs/create/CloneVersionedDataset.kt
@@ -1,6 +1,9 @@
 package org.janelia.saalfeldlab.paintera.ui.dialogs.create
 
+import bdv.export.ProgressWriter
+import bdv.ij.util.ProgressWriterIJ
 import bdv.viewer.Source
+import javafx.beans.property.ReadOnlyDoubleWrapper
 import javafx.event.ActionEvent
 import javafx.scene.control.ButtonType
 import javafx.scene.control.TextField
@@ -8,13 +11,16 @@ import javafx.scene.layout.VBox
 import org.janelia.saalfeldlab.fx.ui.DirectoryField
 import org.janelia.saalfeldlab.fx.ui.Exceptions.Companion.exceptionAlert
 import org.janelia.saalfeldlab.fx.ui.NamedNode.Companion.nameIt
+import org.janelia.saalfeldlab.fx.util.InvokeOnJavaFXApplicationThread
 import org.janelia.saalfeldlab.paintera.Constants
 import org.janelia.saalfeldlab.paintera.state.SourceState
 import org.janelia.saalfeldlab.paintera.ui.PainteraAlerts
+import org.janelia.scicomp.api.VersionedStorageAPI
+import org.janelia.scicomp.lib.VersionedDirectory
+import org.janelia.scicomp.v5.VersionedN5Writer
 import org.slf4j.LoggerFactory
 import java.io.IOException
 import java.lang.invoke.MethodHandles
-import java.nio.file.Path
 
 
 class CloneVersionedDataset(private val currentSource: Source<*>?, vararg allSources: SourceState<*, *>) {
@@ -22,13 +28,15 @@ class CloneVersionedDataset(private val currentSource: Source<*>?, vararg allSou
     private val allSources = allSources.toList()
 
     private val username = TextField().apply { maxWidth = Double.MAX_VALUE }
-    private val projectPath: DirectoryField = DirectoryField("", 100.0)
-    private val localPath = TextField().apply { maxWidth = Double.MAX_VALUE }
+    private val indexPathField: DirectoryField = DirectoryField(System.getProperty("user.home"), 100.0)
+    private val datastorePathField: DirectoryField = DirectoryField(System.getProperty("user.home"), 100.0)
+    private val localPathField : DirectoryField = DirectoryField(System.getProperty("user.home"), 100.0)
 
     private val pane = VBox(
-        nameIt("Username", NAME_WIDTH, true, username),
-        nameIt("Project path", NAME_WIDTH, true, projectPath.asNode()),
-        nameIt("Local path", NAME_WIDTH, true, localPath)
+        nameIt("Username:", NAME_WIDTH, true, username),
+        nameIt("Versioned Index Path:", NAME_WIDTH, true, indexPathField.asNode()),
+        nameIt("Data path:", NAME_WIDTH, true, datastorePathField.asNode()),
+        nameIt("Local path:", NAME_WIDTH, true, localPathField.asNode())
     )
 
     fun showDialog(projectDirectory: String?): String {
@@ -38,16 +46,26 @@ class CloneVersionedDataset(private val currentSource: Source<*>?, vararg allSou
             dialogPane.content = pane
             dialogPane.lookupButton(ButtonType.OK).addEventFilter(ActionEvent.ACTION) { e: ActionEvent ->
                 val username = username.text
-                val projectPath = projectPath.directoryProperty().value!!.absolutePath
-                val localPath = localPath.text
-print("hello")
+                val indexPath = indexPathField.directoryProperty().value!!.absolutePath
+                val dataPath = datastorePathField.directoryProperty().value!!.absolutePath
+                val localPath = localPathField.directoryProperty().value!!.absolutePath
                 try {
-                    LOG.debug("Trying to clone project `{}'", projectPath)
-                    if (projectPath.isNullOrEmpty()) throw IOException("Project Path not specified!")
+                    LOG.debug("Trying to clone project index: `{}' data:`{}' to `{}' ", indexPath,dataPath,localPath)
+                    if (indexPath.isNullOrEmpty()) throw IOException("Index Path not specified!")
+                    if (dataPath.isNullOrEmpty()) throw IOException("Data Path not specified!")
                     if (localPath.isNullOrEmpty()) throw IOException("Local Path not specified!")
                     if (username.isNullOrEmpty()) throw IOException("Username not specified!")
                     // TODO in thread and waiting gui
-//                    VersionedStorageAPI.cloneProject(projectPath, localPath, username)
+
+                    val progress = ReadOnlyDoubleWrapper(this, "progress", 0.0)
+                    InvokeOnJavaFXApplicationThread.invoke(Runnable { progress.set(0.1) })
+                    val progressWriter: ProgressWriter = ProgressWriterIJ()
+                    progressWriter.out().println("starting export...")
+                    val writer = VersionedN5Writer.cloneFrom(indexPath,localPath,dataPath,username)
+                    InvokeOnJavaFXApplicationThread.invoke(Runnable { progress.set(1.0) })
+                    progressWriter.setProgress(1.0)
+
+
                     // TODO set in the state the project path
                 } catch (ex: IOException) {
                     LOG.error("Unable to create empty dataset", ex)
@@ -56,8 +74,8 @@ print("hello")
                 }
             }
         }.showAndWait()
-
-        return localPath.text
+//TODO propagate dataset
+        return ""
     }
 
     companion object {
